abbcdf76df85e15639801b5c96af76fcfb51bdf3
IcedTeaTech <icedteatech@gmail.com>
2020-10-12 12:50:38 +0200

    file setup

---
+++ finished_exercises/finished_exercises.js
@@ -0,0 +1,7 @@
+// 1. Assign your first name to a variable called firstName
+
+// 2. Assign your last name to a variable called lastName
+
+// 3. Print both variables to the console on two different lines
+
+// BONUS: Try printing your full name to the console on one line (you can do this in two different ways)
---
+++ script.js
@@ -0,0 +1,18 @@
+// Variables
+
+
+// Printing to the console
+
+/* ***********************
+*                        *
+*       EXERCISES        *
+*                        *
+*********************** */
+
+/*
+    1. Assign your first name to a variable called firstName
+    2. Assign your last name to a variable called lastName
+    3. Print both variables to the console on two different lines
+    BONUS: Try printing your full name to the console on one line (you can do this in two different ways, try some things or use the web
+           to find out how this can be done. Search term: "string concatenation JavaScript")
+*/
