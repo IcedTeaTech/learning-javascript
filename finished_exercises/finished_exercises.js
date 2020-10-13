b93b8b981098b8e124cc86c5f25cd1c4684fc562
IcedTeaTech <icedteatech@gmail.com>
2020-10-13 09:33:56 +0200

    finished exercises

---
+++ finished_exercises/finished_exercises.js
@@ -0,0 +1,15 @@
+// 1. Assign your name to a variable called myName.
+let myName = "Jesse Kroon";
+// 2. Assign your age to a variable called myAge.
+let myAge = 27;
+
+// 3. Using console.log(), print the type of both myName and myAge to the console.
+console.log(myName);
+console.log(myAge);
+
+// 4. Declare two variables called firstNumber and secondNumber and assign a number to both.
+let firstNumber = 10;
+let secondNumber = 29;
+
+// 5. Using the > operator and the variables firstNumber and secondNumber, console log an expression that will be "true".
+console.log(secondNumber > firstNumber);
---
+++ script.js
@@ -0,0 +1,46 @@
+// PRIMITIVE DATA TYPES
+
+// number
+// >9007199254740991 is a BigInt. Specify an 'n' at the end of the number to define a BigInt.
+let intNumber = 4;
+let floatNumber = 4.87;
+console.log(intNumber);
+console.log(floatNumber);
+
+
+// string
+let firstString = 'I have single quotes';
+let secondString = "Hey that's mine";
+let myFavoriteAnimal = 'dog';
+let thirdString = `My favorite animal is ${3 / 4}`;
+
+console.log(thirdString);
+
+
+// boolean
+let firstBool = true;
+let secondBool = false;
+
+console.log(firstBool);
+console.log(secondBool);
+
+console.log(4 > 6);
+
+
+// Checking the type of a value
+console.log(typeof(firstString));
+
+
+/* ***********************
+*                        *
+*       EXERCISES        *
+*                        *
+*********************** */
+
+/* 
+    1. Assign your name to a variable called myName.
+    2. Assign your age to a variable called myAge.
+    3. Using console.log(), print the type of both myName and myAge to the console.
+    4. Declare two variables called firstNumber and secondNumber and assign a number to both.
+    5. Using the > operator, console log an expression that will be "true".
+*/
